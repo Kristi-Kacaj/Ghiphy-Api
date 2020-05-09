6b7dfbf86c0cd57f97b92ca09136b8f6a578c72a
Kristi Kacaj <kristik.web@gmail.com>
2020-05-09 08:51:11 -0400

    Created function to generate images from API and added ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,4 +14,43 @@ $(document).ready(function() {
             $(areaAddedTo).append(button);
         }
     }
+
+    //Function generates gif images from Ghiphy API
+    $(document).on("click", ".cartoon-buttons", function() {
+        $("#images").empty();
+        $(".cartoon-buttons").removeClass("active");
+        $(this).addClass("active");
+
+        let type = $(this).attr("data-type");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q" + type + "&api_key=vU4389qg2slcm6FN6k6M3uokVKmfAW4v=10";
+
+        //Ajax Call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            let results = response.data;
+
+            for (let i = 0; i < results.length; i++) {
+                let cartoonDiv = $("<div class=\"cartoon-shows\">");
+                let rating = results[i].rating;
+                let p = $("<p>").text("Rating: " + rating);
+                let animated = results[i].images.fixed_height.url;
+                let still = results[i].images.fixed_height;
+
+                let cartoonImages = $("<img>");
+                cartoonImages.attr("scr", still);
+                cartoonImages.attr("data-still", still);
+                cartoonImages.attr("data-animated", animated);
+                cartoonImages.attr("data-state", "still");
+                cartoonImages.addClass("cartoon-images");
+
+                cartoonDiv.append(button);
+                cartoonDiv.append(cartoonImages);
+                $("#images").append(cartoonDiv);
+            }
+        })
+    })
 })
+
+    //Changes from still to animated, back and forth, when an image is clicked on.
